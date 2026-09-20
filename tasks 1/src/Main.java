0c6fcd91bfd51f76184692604468e407f0762097
Blindstreet <konakovmaks73@gmail.com>
2026-09-20 23:23:19 +0300

    Add files via upload

---
+++ tasks 1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double rubles = 30002.7;
+        double rateUSD = 78.5;
+        double dollars = rubles / rateUSD;
+
+        System.out.println("У вас на счету " + rubles + " рублей. В долларах это "
+                + dollars + " долларов. Так держать!");
+    }
+}
---
+++ tasks 1/src/task3.java
@@ -0,0 +1,12 @@
+public class task3 {
+    public static void main(String[] args) {
+        int eggs = 5;
+
+        if (eggs > 3) {
+            System.out.println("Рекомендую приготовить омлет.");
+        } else {
+            System.out.println("Рекомендую позавтракать бутербродами.");
+        }
+        System.out.println("Приятного аппетита!");
+    }
+}
